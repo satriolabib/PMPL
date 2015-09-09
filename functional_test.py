6172fc797b4ad606aec0f97446d74304371e6dd0
satriolabib <satrio.labib@ui.ac.id>
2015-09-09 16:23:17 +0700

    Add html web pribadi

--- functional_test.py
+++ functional_test.py
@@ -13,9 +13,9 @@ class NewVisitorTest(unittest.TestCase):
 	def test_can_start_a_list_and_retrieve_it_later(self):
 		self.browser.get('http://localhost:8000')
 
-		self.assertIn('To-Do', self.browser.title)
+		self.assertIn('Web Pribadi', self.browser.title)
 		header_text = self.browser.find_element_by_tag_name('h1').text
-		self.assertIn('To-Do', header_text)
+		self.assertIn('Satrio Labib Mubarak', header_text)
 
 if __name__ == '__main__':
 	unittest.main(warnings='ignore')
--- lists/tests.py
+++ lists/tests.py
@@ -17,5 +17,5 @@ class HomePageTest(TestCase):
 		expected_html = render_to_string('home.html')
 		self.assertEqual(response.content.decode(), expected_html)
 		self.assertTrue(response.content.startswith(b'<html>'))
-		self.assertIn(b'<title>To-Do lists</title>', response.content)
+		self.assertIn(b'<title>Web Pribadi</title>', response.content)
 		self.assertTrue(response.content.strip().endswith(b'</html>'))
