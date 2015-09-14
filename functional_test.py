a605d0d427d407657d212f59be98ba25961cea34
satriolabib <satrio.labib@ui.ac.id>
2015-09-14 18:14:03 +0700

    change functional test, home.html, test.py and views.py

--- functional_test.py
+++ functional_test.py
@@ -28,14 +28,23 @@ class NewVisitorTest(unittest.TestCase):
 			'Enter a to-do item'		
 		)
 
+		inputbox.send_keys('Use peacock feathers to make a fly')
 		inputbox.send_keys(Keys.ENTER)
+
+		#import time
+		#time.sleep(10)
 
 		table = self.browser.find_element_by_id('id_list_table')
 		rows = table.find_elements_by_tag_name('tr')
-		self.assertTrue(
-			any(row.text == '1: Buy peacock feathers' for row in rows),
-			"New to-do item did not appear in table"	
-		)
+		
+		self.assertIn('1: Buy peacock feathers', [row.text for row in rows])		
+		self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])		
+
+		#self.assertTrue(
+		#	any(row.text == '1: Buy peacock feathers' for row in rows),
+		#	"New to-do item did not appear in table -- its text was:\n%s" 			#	% (table.text,)	
+		#)
+		self.fail('Finish the test')
 if __name__ == '__main__':
 	unittest.main(warnings='ignore')
 
