09a3c2a50396e369ae0287fb3bf4eaf03307db02
satriolabib <satrio.labib@ui.ac.id>
2015-10-07 17:16:14 +0700

    Latihan 4

--- lists/views.py
+++ lists/views.py
@@ -8,11 +8,26 @@ def home_page(request):
 		Item.objects.create(text=request.POST['item_text'])
 		return redirect('/lists/the-only-list-in-the-world/')
 	items = Item.objects.all()
-	return render(request, 'home.html')
+	#
+
+	#if (count == 0):
+	comment ='Yey, waktunya berlibur'
+	#el
+	#	
+	#
+	
+	return render(request, 'home.html', {'items': items, 'comment': comment})
 
 def view_list(request, list_id):
 	list_ = List.objects.get(id=list_id)
-	return render(request, 'list.html', {'list': list_})
+	count = Item.objects.filter(list=list_).count()	
+	
+	if (count < 5):
+		comment ='sibuk tapi santai'
+	elif (count >= 5):
+		comment ='oh tidak'
+
+	return render(request, 'list.html', {'list': list_, 'comment': comment})
 
 def new_list(request):
 	list_ = List.objects.create()
